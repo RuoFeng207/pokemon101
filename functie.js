3f73e4c8700a5a0d1ad9bc82d31682cb3d6ebe28
RuoFeng207 <99081157@mydavinci.nl>
2025-03-04 10:12:05 +0100

    update werkende knop

---
+++ functie.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.getElementById("catch_them_all");
+    const audio = document.getElementById("pokemonAudio");
+
+    button.addEventListener("click", function () {
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+            audio.currentTime = 0;
+        }
+    });
+});
+
+document.addEventListener("DOMContentLoaded",function(){
+    const button = document.getElementById("start");
+    button.addEventListener("click", function() {
+        alert("hallo world")
+
+    });
+});
