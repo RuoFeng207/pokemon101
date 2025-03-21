c117f9c8ce6283601039d567b7e48788bf834f5f
RuoFeng207 <99081157@mydavinci.nl>
2025-03-21 09:48:52 +0100

    menu update

---
+++ menu.js
@@ -0,0 +1,10 @@
+// fetch("menu.html")
+//             .then(response => response.text())
+//             .then(data => {
+//                 document.getElementById('menuContainer').innerHTML = data;
+                
+//                 document.getElementById('menuToggle').addEventListener('click', function() {
+//                     var menu = document.getElementById('menu');
+//                     menu.classList.toggle('active');
+//                 });
+//             });
